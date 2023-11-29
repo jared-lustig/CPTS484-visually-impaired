b9bbcc663b00ee29a009a4838c6dfeeb30d65ba5
jared-lustig <jared.lustig@wsu.edu>
2023-11-28 21:26:50 -0800

    fixed web navigation

--- Theia/App.js
+++ Theia/App.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
 import Tabs from './src/navigation/Tabs';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
--- Theia/src/navigation/Tabs.js
+++ Theia/src/navigation/Tabs.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import HomeScreen from '../screens/HomeScreen';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from '../screens/HomeScreen';
 
 const Tab = createMaterialBottomTabNavigator();
 
-export default function Tabs() {
+function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen  name="Home" component={HomeScreen} />
-      <Text>Beans</Text>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
+
+export default Tabs;

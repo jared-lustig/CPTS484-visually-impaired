7bf884cad31072566aa3b267f4777ae31bfedb14
jared-lustig <jared.lustig@wsu.edu>
2023-12-07 20:42:32 -0800

    maps component made

--- Theia/src/screens/MapScreen.js
+++ Theia/src/screens/MapScreen.js
@@ -1,19 +1,28 @@
-import * as React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Image, View} from "react-native";
 
-import { View } from "../components/Themed";
-import { FlatListComponent } from "../components/FlatList";
+import DropdownComponent from "../components/DropDownComponent";
 
 export default function MapScreen() {
+  const maps = require.context('../../assets/maps/', true);
+  const mapNames = maps.keys();
+  const mapList = maps.keys().map(image => maps(image));
+
+  let mapKeys = [];
+  mapNames.map((map, index) => mapKeys.push({label: map.replace("./", "").replace("_", " ").replace(".png", ""), value: index.toString()}))
+
+  const [value, setValue] = useState(mapKeys[0].value);
+
+  console.log(value)
+
   return (
-    <View style={styles.container}>
-      <FlatListComponent />
+    <View>
+      <Image 
+      style={{width: 500, height: 300, 
+              display: 'block', marginLeft: 'auto',
+              marginRight:'auto'}} 
+      source={mapList[value]} />
+      <DropdownComponent data={mapKeys} value={value} setValue={setValue}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- Theia/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-/*
-reference(s): 
-https://github.com/claysimps/RN-swiper-components/tree/FlatList
-https://blog.logrocket.com/swiper-components-react-native/
-*/ 
-
-export { DATA } from "./GenerateImages";
